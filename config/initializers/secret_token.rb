62924b1bd3ffffb9718d027f7b5671f4176f0a87
Jake Schoermer <jake@secretservers.net>
2016-04-07 13:28:54 +1000

    Generate new clean rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ValidatorService::Application.config.secret_key_base = '1736b2b831416582a64937461cbf7fc93b34163550a03dbc802f7127246aa03e5cb07bc53be66ae99b12c39c3769ed4f7259d2e30b0a223fca76535ddfd4b4ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ValidatorService::Application.config.session_store :cookie_store, key: '_validator-service_session'
