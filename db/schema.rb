cbc65ca395e06d74603ddc0e0f789754520aad50
Jake Schoermer <jake@secretservers.net>
2016-04-14 11:59:05 +1000

    Add mysql socked config to codeship.database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407062155) do
+ActiveRecord::Schema.define(version: 20160411012331) do
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.string   "mail",       limit: 255, null: false
+    t.boolean  "enabled",                null: false
+    t.boolean  "complete",               null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
