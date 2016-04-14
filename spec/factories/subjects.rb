514deb2135813491b4cea1cbb5b7c7a2b29122b4
Jake Schoermer <jake@secretservers.net>
2016-04-14 16:50:02 +1000

    Implement application controller and specs from gumboot

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -5,5 +5,16 @@ FactoryGirl.define do
     mail { Faker::Internet.email }
     enabled { true }
     complete { true }
+
+    trait :authorized do
+      transient { permission '*' }
+
+      after(:create) do |subject, attrs|
+        role = create :role
+        permission = create :permission, value: attrs.permission, role: role
+        role.permissions << permission
+        role.subjects << subject
+      end
+    end
   end
 end
