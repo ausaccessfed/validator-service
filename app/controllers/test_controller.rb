34732071ed3d43ce9fa7720e69b4a24afd8b7ea7
Jake Schoermer <jake@secretservers.net>
2016-04-21 11:50:08 +1000

    Made test_controller#test action public.
    Skipping the authentication action so that the root page is
    unprotected.

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # Class for the temporary test Controller
 class TestController < ApplicationController
+  skip_before_action :ensure_authenticated
   def test
     public_action
   end
