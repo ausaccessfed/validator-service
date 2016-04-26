1256a569134a5736cd89d9da6eaa4ec3f68ae109
Jake Schoermer <jake@secretservers.net>
2016-04-27 09:22:20 +1000

    Restore application_controller and subject receiver
    Restored standard application controller and removed test code
    trom subject receiver.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+# Class for the temporary dashboard Controller
 class DashboardController < ApplicationController
 
   def dashboard
+    public_action
   end
 
 end
--- lib/authentication/subject_receiver.rb
+++ lib/authentication/subject_receiver.rb
@@ -7,7 +7,7 @@ module Authentication
     map_single_value shared_token:           'HTTP_AUEDUPERSONSHAREDTOKEN',
                      targeted_id:            'HTTP_TARGETED_ID',
                      principal_name:         'HTTP_PRINCIPALNAME',
-                     # name:                   'HTTP_DISPLAYNAME',
+                     name:                   'HTTP_DISPLAYNAME',
                      display_name:           'HTTP_DISPLAYNAME',
                      cn:                     'HTTP_CN',
                      mail:                   'HTTP_MAIL',
@@ -19,9 +19,6 @@ module Authentication
                      scoped_affiliation:     'HTTP_EDUPERSONSCOPEDAFFILIATION'
 
     def subject(_env, attrs)
-      puts "============"
-      puts attrs
-      puts "============"
       Subject.transaction do
         identifier = attrs.slice(:targeted_id)
         subject = Subject.find_or_initialize_by(identifier)
@@ -29,15 +26,6 @@ module Authentication
         # This is a temporary hack
         subject.enabled = true
         subject.complete = true
-        subject.name = 'Some Bozo'
-        subject.mail = attrs[:mail]
-        subject.shared_token = attrs[:shared_token]
-        subject.principal_name = attrs[:principal_name]
-        subject.display_name = attrs[:display_name]
-        subject.cn = attrs[:cn]
-        subject.o = attrs[:o]
-        subject.home_organization = attrs[:home_organization]
-        subject.home_organization_type = attrs[:home_organization_type]
 
         ensure_subject_match(subject, attrs)
         subject.save(validate: false)
