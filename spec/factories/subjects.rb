1b54a5ddd4f12817e43f7f7d90cb69c1603f398c
Jake Schoermer <jake@secretservers.net>
2016-04-27 10:27:59 +1000

    Fix failing rspec tests
    Fixed failing rspec tests by:
    
    * Adding relevant fields to subjects factory
    * removing superfluous dashboard helper and spec

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 FactoryGirl.define do
+
+  idp_domain = Faker::Internet.domain_name
+  idp = "https://idp.#{idp_domain}/idp/shibboleth"
+  sp = "https://sp.#{Faker::Internet.domain_name}/shibboleth"
+  name = Faker::Name.name
+
   factory :subject do
     name { Faker::Name.name }
     mail { Faker::Internet.email }
+
+    shared_token { SecureRandom.urlsafe_base64(20) }
+    targeted_id { "#{idp}!#{sp}!#{SecureRandom.uuid}" }
+    principal_name { "#{name}" }
+    display_name { "#{name}" }
+    cn { "#{name}" }
+    o { Faker::Company.name }
+    home_organization { "ernser.example.edu" }
+    home_organization_type { "urn:mace:terena.org:schac:homeOrganizationType:au:university" }
+
     enabled { true }
     complete { true }
 
