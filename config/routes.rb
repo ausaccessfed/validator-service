26ca51d2439788a43dc041551e6b814dbea68d00
Jake Schoermer <jake@secretservers.net>
2016-05-05 09:28:52 +1000

    Fix whitespace issue in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 require 'api_constraints'
 
 ValidatorService::Application.routes.draw do
-
   root to: 'welcome#index'
   get '/welcome' => 'welcome#index'
   get '/dashboard' => 'dashboard#dashboard'
