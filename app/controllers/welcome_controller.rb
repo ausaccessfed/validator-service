df9e0c86a7c0a53cfd38cfef92b5acaf75538626
Jake Schoermer <jake@secretservers.net>
2016-05-05 10:02:57 +1000

    Update outdated comment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# Class for the temporary test Controller
+# Class for the Welcome Controller
 class WelcomeController < ApplicationController
   skip_before_action :ensure_authenticated
   def index
