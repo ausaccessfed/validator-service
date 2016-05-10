e42a3846550deeea4faad6ec940475d5164e7fec
Jake Schoermer <jake@secretservers.net>
2016-05-10 12:12:50 +1000

    Add model specs for attribute_values

---
+++ spec/models/attribute_value_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'Attribute Values', type: :model do
+  let(:attribute_value) { build :attribute_value}
+
+  it { expect(attribute_value).to be_valid }
+
+  it 'is invalid without a value' do
+    attribute_value.value = nil
+    expect(attribute_value).not_to be_valid
+  end
+end
