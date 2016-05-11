5ac6a686831fe3e525b89a9f733923f39ad6a0c9
Jake Schoermer <jake@secretservers.net>
2016-05-12 09:26:38 +1000

    Change attriute_value has_many snapshot to has_one

--- app/models/attribute_value.rb
+++ app/models/attribute_value.rb
@@ -2,7 +2,7 @@
 # Class defining AttributeValue model
 class AttributeValue < ActiveRecord::Base
   has_many :snapshot_attribute_values
-  has_many :snapshots, through: :snapshot_attribute_values
+  has_one :snapshots, through: :snapshot_attribute_values
 
   belongs_to :federation_attribute
 
