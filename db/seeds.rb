626b250bc452f7c7f8ee3a56526ab9d1c009796b
Jake Schoermer <jake@secretservers.net>
2016-05-12 16:48:46 +1000

    Create default FederationAttributes in seeds

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# Federation Attributes
+FederationAttribute.create ({ name: 'targeted_id' })
+FederationAttribute.create ({ name: 'shared_token' })
+FederationAttribute.create ({ name: 'principal_name' })
+FederationAttribute.create ({ name: 'name' })
+FederationAttribute.create ({ name: 'display_name' })
+FederationAttribute.create ({ name: 'cn' })
+FederationAttribute.create ({ name: 'mail' })
+FederationAttribute.create ({ name: 'o' })
+FederationAttribute.create ({ name: 'home_organization' })
+FederationAttribute.create ({ name: 'home_organization_type' })
+
+FederationAttribute.create ({
+  name: 'affiliation',
+  singular: false
+})
+
+FederationAttribute.create ({
+  name: 'scoped_affiliation',
+  singular: false
+})
