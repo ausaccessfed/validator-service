8613b0c75a7af34ea583e82a437cffbb17efe37d
Jake Schoermer <jake@secretservers.net>
2016-05-13 13:50:58 +1000

    Refactor shib_attrs factory
    Moved valid_affiliations to a shared variable to avoid repetition.

--- spec/factories/shib_attrs.rb
+++ spec/factories/shib_attrs.rb
@@ -4,6 +4,15 @@ FactoryGirl.define do
   idp = "https://idp.#{idp_domain}/idp/shibboleth"
   sp = "https://sp.#{Faker::Internet.domain_name}/shibboleth"
   name = Faker::Name.name
+  valid_affiliations = [
+    'faculty',
+    'student',
+    'staff',
+    'employee',
+    'member',
+    'affiliate',
+    'alum',
+    'library-walk-in']
 
   factory :shib_attrs, class: Hash do
     shared_token { SecureRandom.urlsafe_base64(20) }
@@ -20,15 +29,6 @@ FactoryGirl.define do
     end
     affiliation do
       affiliations = []
-      valid_affiliations = [
-        'faculty',
-        'student',
-        'staff',
-        'employee',
-        'member',
-        'affiliate',
-        'alum',
-        'library-walk-in']
       rand(2...10).times do
         affiliations << valid_affiliations.sample
       end
@@ -36,15 +36,6 @@ FactoryGirl.define do
     end
     scoped_affiliation do
       scoped_affiliations = []
-      valid_affiliations = [
-        'faculty',
-        'student',
-        'staff',
-        'employee',
-        'member',
-        'affiliate',
-        'alum',
-        'library-walk-in']
       rand(2...10).times do
         scoped_affiliations << "#{valid_affiliations.sample}@#{idp_domain}"
       end
