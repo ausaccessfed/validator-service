9c10559e859ae8078c43a74a4ffae14a59803bd2
Ryan Caught <rcaught@gmail.com>
2016-06-16 13:05:42 +1000

    Group test config settings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,4 +15,12 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
 end
