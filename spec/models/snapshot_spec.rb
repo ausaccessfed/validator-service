18186f58601213dbdb26d6c2a33547bfde8b7427
Ryan Caught <rcaught@gmail.com>
2016-07-01 15:44:22 +1000

    Fix snapshot spec

--- spec/models/snapshot_spec.rb
+++ spec/models/snapshot_spec.rb
@@ -7,7 +7,17 @@ RSpec.describe Snapshot, type: :model do
   it { expect(snapshot).to be_valid }
 
   describe '.create_from_receiver' do
-    let(:attrs) { build(:shib_attrs) }
+    let(:attrs) do
+      Authentication::AttributeHelpers
+        .federation_attributes(attributes_for(:shib_env)[:env])
+    end
+
+    before :each do
+      %w(HTTP_TARGETED_ID HTTP_MAIL HTTP_DISPLAYNAME).each do |http_header|
+        create(:federation_attribute, http_header: http_header)
+      end
+    end
+
     let(:subject) { Subject.create(attributes_for(:subject)) }
     let(:snapshot) { Snapshot.create_from_receiver(subject, attrs) }
 
@@ -21,7 +31,7 @@ RSpec.describe Snapshot, type: :model do
 
     it 'creates a new attribute value record for each attr passed in' do
       snapshot.reload.attribute_values.each do |av|
-        expect(av.value).to eql(attrs[av.federation_attribute.name.to_sym])
+        expect(av.value).to eql(attrs[av.federation_attribute.http_header])
       end
     end
   end
