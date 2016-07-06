480354a5670486ec890c446f75834911b3672fc4
Ryan Caught <rcaught@gmail.com>
2016-07-06 15:23:57 +1000

    Creates a snapshot

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,11 +2,19 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-  let(:subject_obj) { create(:subject) }
+  let(:subject) { create(:subject) }
+  let(:attrs) do
+    Authentication::AttributeHelpers
+      .federation_attributes(attributes_for(:shib_env)[:env])
+  end
+  let(:has_snapshot) { Snapshot.create_from_receiver(subject, attrs) }
+
   describe '#dashboard' do
     context 'when the user is logged in' do
       before do
-        session[:subject_id] = subject_obj.id
+        has_snapshot
+
+        session[:subject_id] = subject.id
         get :dashboard
       end
 
