c8652ca4631a1ba3dd2d9fcb1f395b49f9e9501d
Ryan Caught <rcaught@gmail.com>
2016-07-08 16:07:34 +1000

    Some extra coverage on the instance variables

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
   let(:subject) { create(:subject) }
+
   let(:attrs) do
     Authentication::AttributeHelpers
       .federation_attributes(attributes_for(:shib_env)[:env])
   end
-  let(:has_snapshot) { Snapshot.create_from_receiver(subject, attrs) }
+
+  let!(:category) { create(:category) }
+
+  let(:has_snapshot) do
+    %w(HTTP_TARGETED_ID HTTP_MAIL HTTP_DISPLAYNAME).each do |http_header|
+      create(:federation_attribute, http_header: http_header)
+    end
+
+    Snapshot.create_from_receiver(subject, attrs)
+  end
 
   describe '#dashboard' do
     context 'when the user is logged in' do
@@ -20,6 +30,17 @@ RSpec.describe DashboardController, type: :controller do
 
       it 'should render ok' do
         expect(response).to have_http_status(:ok)
+      end
+
+      it 'assigns' do
+        expect(assigns(:latest_snapshot)).to be_instance_of(Snapshot)
+
+        expect(assigns(:snapshot_values).size).to eql 3
+        expect(assigns(:snapshot_values).first)
+          .to be_instance_of(AttributeValue)
+
+        expect(assigns(:categories).size).to eql 1
+        expect(assigns(:categories).first).to be_instance_of(Category)
       end
 
       it { is_expected.to render_template('dashboard/dashboard') }
