651b9d6f1de6a24867a718305fd1ca33f47d032a
Ryan Caught <rcaught@gmail.com>
2016-07-18 11:04:12 +1000

    Routes reordering

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  mount ShibRack::Engine => '/auth'
+
   root to: 'welcome#index'
+
   get '/welcome' => 'welcome#index'
   get '/dashboard' => 'dashboard#dashboard'
   get '/documentation' => 'documentation#index'
 
-  mount ShibRack::Engine => '/auth'
   namespace :documentation do
     resources :attributes, only: [:index, :show]
     resources :categories, only: [:index, :show]
