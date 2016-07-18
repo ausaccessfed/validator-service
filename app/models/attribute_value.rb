d2a98bb3bc4c32de572ddbe7bacec9f0aedbad0f
Ryan Caught <rcaught@gmail.com>
2016-07-18 10:54:11 +1000

    Attribute validation and display
    Handles valid, invalid, required and imperfect validation states.
    Displays them to the user.

--- app/models/attribute_value.rb
+++ app/models/attribute_value.rb
@@ -6,4 +6,107 @@ class AttributeValue < ActiveRecord::Base
   belongs_to :federation_attribute
 
   valhammer
+
+  class << self
+    def validation_state(category, federation_attribute, attribute_value)
+      if !attribute_value
+        required_response(category, federation_attribute)
+      else
+        valid_response(federation_attribute, attribute_value)
+      end
+    end
+
+    def valid_response(federation_attribute, attribute_value)
+      if federation_attribute.regexp
+        unless Regexp.new(federation_attribute.regexp).match(attribute_value)
+          if federation_attribute.regexp_triggers_failure?
+            return ResponseFor.invalid_attribute
+          else
+            return ResponseFor.imperfect_attribute
+          end
+        end
+      end
+
+      ResponseFor.valid_attribute
+    end
+
+    def required_response(category, federation_attribute)
+      if category.category_attributes
+                 .find_by(federation_attribute: federation_attribute)
+                 .try(:presence?)
+        ResponseFor.required_attribute
+      else
+        ResponseFor.not_supplied_attribute
+      end
+    end
+  end
+
+  class ResponseFor
+    class << self
+      def required_attribute
+        {
+          state: 'error',
+          message: 'This attribute is required for this category.',
+          icon_classes: [
+            'glyphicon',
+            'glyphicon-remove-sign',
+            'alert-danger'
+          ],
+          row_classes: ['danger']
+        }
+      end
+
+      def invalid_attribute
+        {
+          state: 'invalid',
+          message: 'This attribute is invalid.',
+          icon_classes: [
+            'glyphicon',
+            'glyphicon-remove-sign',
+            'alert-danger'
+          ],
+          row_classes: ['danger']
+        }
+      end
+
+      def valid_attribute
+        {
+          state: 'valid',
+          message: 'This attribute forefills all requirements.',
+          icon_classes: [
+            'glyphicon',
+            'glyphicon-ok-sign',
+            'alert-success'
+          ],
+          row_classes: ['success']
+        }
+      end
+
+      def not_supplied_attribute
+        {
+          state: 'not_supplied',
+          message: 'This attribute has not been supplied.',
+          icon_classes: [
+            'glyphicon',
+            'glyphicon-exclamation-sign',
+            'alert-warning'
+          ],
+          row_classes: ['warning']
+        }
+      end
+
+      def imperfect_attribute
+        {
+          state: 'imperfect',
+          message: 'This attribute is not valid, but allowable.',
+          icon_classes: [
+            'glyphicon',
+            'glyphicon-exclamation-sign',
+            'alert-warning'
+          ],
+          row_classes: ['warning']
+        }
+      end
+    end
+  end
 end
--- spec/models/attribute_value_spec.rb
+++ spec/models/attribute_value_spec.rb
@@ -3,11 +3,95 @@ require 'rails_helper'
 
 RSpec.describe 'Attribute Values', type: :model do
   let(:attribute_value) { build :attribute_value }
+  let(:category) { build :category }
+  let(:federation_attribute) do
+    create(:federation_attribute,
+           regexp: '^[a-zA-Z\s\-]+$',
+           regexp_triggers_failure: false,
+           category_attributes: [
+             CategoryAttribute.new(presence: false, category: category)
+           ])
+  end
+  let(:strict_federation_attribute) do
+    create(:federation_attribute,
+           regexp: '^[a-zA-Z\s\-]+$',
+           regexp_triggers_failure: true,
+           category_attributes: [
+             CategoryAttribute.new(presence: false, category: category)
+           ])
+  end
+  let(:required_federation_attribute) do
+    create(:federation_attribute,
+           regexp: '^[a-zA-Z\s\-]+$',
+           regexp_triggers_failure: false,
+           category_attributes: [
+             CategoryAttribute.new(presence: true, category: category)
+           ])
+  end
+  let(:name) { Faker::Name.name }
 
   it { expect(attribute_value).to be_valid }
 
   it 'is invalid without a value' do
     attribute_value.value = nil
     expect(attribute_value).not_to be_valid
+  end
+
+  context 'class' do
+    describe '.validation_state' do
+      it 'has an attribute' do
+        expect(
+          AttributeValue.validation_state(
+            category,
+            federation_attribute,
+            name
+          )
+        ).to eql AttributeValue::ResponseFor.valid_attribute
+      end
+
+      it 'has no attribute' do
+        expect(
+          AttributeValue.validation_state(
+            category,
+            federation_attribute,
+            nil
+          )
+        ).to eql AttributeValue::ResponseFor.not_supplied_attribute
+      end
+    end
+
+    describe '.valid_response' do
+      it 'is valid' do
+        expect(AttributeValue.valid_response(federation_attribute, name))
+          .to eql AttributeValue::ResponseFor.valid_attribute
+      end
+
+      it 'is invalid' do
+        expect(
+          AttributeValue.valid_response(strict_federation_attribute, '123')
+        ).to eql AttributeValue::ResponseFor.invalid_attribute
+      end
+
+      it 'is imperfect' do
+        expect(AttributeValue.valid_response(federation_attribute, 'Name 123'))
+          .to eql AttributeValue::ResponseFor.imperfect_attribute
+      end
+    end
+
+    describe '.required_response' do
+      it 'is required' do
+        expect(
+          AttributeValue.required_response(
+            category,
+            required_federation_attribute
+          )
+        ).to eql AttributeValue::ResponseFor.required_attribute
+      end
+
+      it 'is not supplied' do
+        expect(AttributeValue.required_response(category, federation_attribute))
+          .to eql AttributeValue::ResponseFor.not_supplied_attribute
+      end
+    end
   end
 end
