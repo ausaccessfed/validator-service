926caafed161b8000470ea3cc3885b3e67ede8f7
Ryan Caught <rcaught@gmail.com>
2016-07-18 10:59:48 +1000

    Factory attributes for new fields

--- spec/factories/federated_attributes.rb
+++ spec/factories/federated_attributes.rb
@@ -5,7 +5,9 @@ FactoryGirl.define do
     regexp { Faker::Lorem.word }
     regexp_triggers_failure { Faker::Boolean.boolean }
     description { Faker::Lorem.paragraph }
-    documentation_url { Faker::Internet.url }
+    notes_on_format { Faker::Lorem.paragraph }
+    notes_on_usage { Faker::Lorem.paragraph }
+    notes_on_privacy { Faker::Lorem.paragraph }
     singular { Faker::Boolean.boolean }
     http_header { "HTTP_#{Faker::Lorem.word.upcase}" }
   end
