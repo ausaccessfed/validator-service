94d4e92a95e7a6e8cafe93c0610533beaab6fbdf
Ryan Caught <rcaught@gmail.com>
2016-08-02 11:16:17 +1000

    Migration for aliases and oids

---
+++ db/migrate/20160718013550_create_federation_attribute_aliases.rb
@@ -0,0 +1,17 @@
+class CreateFederationAttributeAliases < ActiveRecord::Migration[5.0]
+  def change
+    create_table :federation_attribute_aliases do |t|
+      t.string :name, null: false
+      t.index :name, unique: true
+
+      t.references :federation_attribute, foreign_key: true, null: false
+
+      t.timestamps
+    end
+
+    add_column :federation_attributes, :oid, :string
+    add_index :federation_attributes, :oid, unique: true
+
+    remove_column :federation_attributes, :name
+  end
+end
