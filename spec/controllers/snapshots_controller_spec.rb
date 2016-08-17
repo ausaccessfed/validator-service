22ffff9c19c42e3da26feef95c13d706e7091f51
Ryan Caught <rcaught@gmail.com>
2016-08-17 11:10:19 +1000

    Cover controller

--- spec/controllers/snapshots_controller_spec.rb
+++ spec/controllers/snapshots_controller_spec.rb
@@ -53,4 +53,53 @@ RSpec.describe SnapshotsController, type: :controller do
       it { is_expected.to render_template('snapshots/show') }
     end
   end
+
+  describe '#show' do
+    context 'when the user is logged in' do
+      before do
+        has_snapshot
+
+        session[:subject_id] = subject.id
+        get :show, params: { id: subject.snapshots.first }
+      end
+
+      it 'should render ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'assigns' do
+        expect(assigns(:snapshot)).to be_instance_of(Snapshot)
+
+        expect(assigns(:attribute_values).size).to eql 3
+        expect(assigns(:attribute_values).first)
+          .to be_instance_of(AttributeValue)
+
+        expect(assigns(:categories).size).to eql 1
+        expect(assigns(:categories).first).to be_instance_of(Category)
+      end
+
+      it { is_expected.to render_template('snapshots/show') }
+    end
+  end
+
+  describe '#index' do
+    context 'when the user is logged in' do
+      before do
+        has_snapshot
+
+        session[:subject_id] = subject.id
+        get :index
+      end
+
+      it 'should render ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'assigns' do
+        expect(assigns(:snapshots).size).to eql 1
+      end
+
+      it { is_expected.to render_template('snapshots/index') }
+    end
+  end
 end
