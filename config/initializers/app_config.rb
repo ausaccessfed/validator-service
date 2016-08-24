5eff81f23fd482a68a0b95479ebb9891572b7b44
Ryan Caught <rcaught@gmail.com>
2016-08-24 11:34:50 +1000

    Setup and config files for IdE
    - Link local cert and key
    - Create config file
    - Use test api cert and key

---
+++ config/initializers/app_config.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require 'mail'
+
+Rails.application.configure do
+  app_config = YAML.load(Rails.root.join('config/validator_service.yml').read)
+  config.validator_service = OpenStruct.new(app_config.deep_symbolize_keys)
+
+  if Rails.env.test?
+    config.validator_service.ide = {
+      host: 'ide.example.edu',
+      cert: 'spec/api.crt',
+      key: 'spec/api.key',
+      admin_entitlements: ['urn:mace:aaf.edu.au:ide:internal:aaf-admin',
+                           'urn:mace:aaf.edu.au:ide:internal:aaf-validator']
+    }
+
+    config.validator_service.url_options = { base_url: 'example.com' }
+
+    config.validator_service.mail = OpenStruct.new(from: 'noreply@example.com')
+    config.validator_service.environment_string = 'Test'
+
+    Mail.defaults { delivery_method :test }
+  end
+end
