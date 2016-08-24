b5d62ffff08f9593ff07f8ed0869e1a75702e4b3
Ryan Caught <rcaught@gmail.com>
2016-08-24 12:17:26 +1000

    Fix lipstick compatibility bugs
    - Configure Kaminari to not conflict with will_paginate
    - Disable form helpers (admin forms are conflicting)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@
 module ApplicationHelper
   include Lipstick::Helpers::LayoutHelper
   include Lipstick::Helpers::NavHelper
-  include Lipstick::Helpers::FormHelper
 
   def application_version
     ValidatorService::Application::VERSION
--- app/models/federation_attribute_alias.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-class FederationAttributeAlias < ApplicationRecord
-  belongs_to :federation_attribute
-end
