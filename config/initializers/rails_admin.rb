698aaeca4117dc7c5a7383639d63994e00cd07bf
Ryan Caught <rcaught@gmail.com>
2016-08-29 12:27:05 +1000

    Provide a message in console and flash notice for non-admin
    - Only in dev and debug, will give a message on how to create an admin
    role.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -19,7 +19,26 @@ RailsAdmin.config do |config|
     ensure_authenticated
     @access_checked = true
 
-    unless subject.roles.any?(&:admin_entitlements?)
+    unless subject.admin?
+      message = <<-message
+        You are not an admin in test IdE.
+
+        To override this for a development user, add a role with an
+        entitlement of `urn:mace:aaf.edu.au:ide:internal:aaf-admin` and
+        add an entry to `subject_roles` linking your subject and new role.
+
+        `rails c` eg.
+        > r = Role.create!(
+            name: 'auto',
+            entitlement: 'urn:mace:aaf.edu.au:ide:internal:aaf-admin')
+        > s = Subject.find_by(mail: 'jefferey.kohler@ernser.example.edu')
+        > s.roles << r
+      message
+
+      Rails.logger.debug message
+
+      flash[:notice] = simple_format(message) if Rails.env.development?
+
       redirect_to controller.main_app.root_path
     end
   end
