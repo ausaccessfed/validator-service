3f0a7f470444ab17b0bb78dc80ab5054013c4567
Ryan Caught <rcaught@gmail.com>
2016-09-09 14:32:33 +1000

    Fix environment matching in Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,12 +2,13 @@
 
 config = YAML.load_file(File.expand_path('../deploy.yml', __FILE__))
 puma_config = config['puma']
+env = ENV.fetch('RAILS_ENV') { 'development' }
 
 preload_app!
-daemonize unless ENV.fetch('RAILS_ENV') == 'development'
+daemonize unless env == 'development'
 
 port puma_config['port']
-environment ENV.fetch('RAILS_ENV') { 'development' }
+environment env
 workers 2
 threads 8, 32
 tag 'validator'
