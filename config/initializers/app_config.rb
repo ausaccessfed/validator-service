af9b92479f495a8bfea5de0f9eba480257092f6d
Ryan Caught <rcaught@gmail.com>
2016-09-28 15:03:32 +1000

    Configure asset_host to get PDF generation working
    PDF generation needs full URL’s for generation, so we need to set an
    asset_host config value that comes from the ansible config.
    
    Moving stuff into the application config as it makes more sense here.

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  app_config = YAML.load(Rails.root.join('config/validator_service.yml').read)
-  config.validator_service = OpenStruct.new(app_config.deep_symbolize_keys)
-
   if Rails.env.test?
     config.validator_service.url_options = { base_url: 'example.com' }
 
