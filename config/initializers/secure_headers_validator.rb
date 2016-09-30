03e375c6881230b22d20dc1b50d9d058f6a76ca7
Ryan Caught <rcaught@gmail.com>
2016-09-30 15:25:19 +1000

    Localised configuration of secure headers
    - Remove Google fonts back into base config
    - Object exception for PDF’s
    - CSP and HPKP mostly loaded from Ansible based config

--- config/initializers/secure_headers_validator.rb
+++ config/initializers/secure_headers_validator.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 # :nocov:
 if Rails.env.production?
+  app_config = Rails.application.config.validator_service
+
   SecureHeaders::Configuration.override(:default) do |config|
-    config.csp[:style_src] << 'https://fonts.googleapis.com'
     config.csp[:style_src] << "'unsafe-inline'"
-    config.csp[:font_src] << 'https://fonts.gstatic.com'
+    config.csp[:object_src] << "'self'"
+
+    config.csp[:report_uri] << app_config.secure_headers[:csp][:report_uri]
+
+    config.hpkp = {
+      report_only: false,
+      max_age: 60.days.to_i,
+      include_subdomains: true,
+      report_uri: app_config.secure_headers[:hpkp][:report_uri]
+      pins: [
+        {
+          sha256: app_config.secure_headers[:hpkp][:pins].first
+        },
+        {
+          sha256: app_config.secure_headers[:hpkp][:pins].second
+        }
+      ]
+    }
   end
 end
 # :nocov:
