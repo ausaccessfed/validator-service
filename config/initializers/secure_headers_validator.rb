9ae5adff99db7ed1ff69a49f704dd6835699b0be
Ryan Caught <rcaught@gmail.com>
2016-10-04 10:44:32 +1000

    Remove HPKP

--- config/initializers/secure_headers_validator.rb
+++ config/initializers/secure_headers_validator.rb
@@ -8,14 +8,6 @@ if Rails.env.production?
     config.csp[:object_src] << "'self'"
 
     config.csp[:report_uri] << app_config.secure_headers[:csp][:report_uri]
-
-    config.hpkp = {
-      report_only: false,
-      max_age: 60.days.to_i,
-      include_subdomains: true,
-      report_uri: app_config.secure_headers[:hpkp][:report_uri],
-      pins: app_config.secure_headers[:hpkp][:pins].map { |h| { sha256: h } }
-    }
   end
 end
 # :nocov:
