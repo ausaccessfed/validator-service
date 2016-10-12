df1282f8bedec441cfdc9056bb96714349fe71f0
Ryan Caught <rcaught@gmail.com>
2016-10-12 16:06:35 +1000

    Fix CSP's
    - Split into Validator CSP's and RailsAdmin CSP's
    - Add connect_src and some extra script_src's (unfortunately unsafe-eval)
    - Load the RailsAdmin CSP on any RailsAdmin Controller calls
    
    Nocov apply_rails_admin_csp
    
    Rubocop fixes

--- config/initializers/secure_headers_validator.rb
+++ config/initializers/secure_headers_validator.rb
@@ -4,9 +4,7 @@ if Rails.env.production?
   app_config = Rails.application.config.validator_service
 
   SecureHeaders::Configuration.override(:default) do |config|
-    config.csp[:style_src] << "'unsafe-inline'"
     config.csp[:object_src] = ["'self'"]
-
     config.csp[:report_uri] << app_config.secure_headers[:csp][:report_uri]
   end
 end
