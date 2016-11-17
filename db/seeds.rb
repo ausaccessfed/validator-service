fbd5bf166480fc65751dde0e72c267a362850ecb
Bradley Beddoes <bradleybeddoes@gmail.com>
2016-11-17 14:03:37 +1000

    Seed Persistent NameID attribute

--- db/seeds.rb
+++ db/seeds.rb
@@ -567,6 +567,72 @@ The eduPerson specification requires that a value of eduPersonTargetedID once
    attribute.'
 )
 
+faa = FederationAttributeAlias.create!(
+  name: 'Persistent NameID'
+)
+
+FederationAttribute.create!(
+  oid: 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+  internal_alias: 'persistent_id',
+  http_header: 'HTTP_PERSISTENT_ID',
+  federation_attribute_aliases: [faa],
+  primary_alias: faa,
+  regexp:
+    '\A(?=[\S]{0,256}$)[^!]+![^!]+![^!]+\z',
+  regexp_triggers_failure: true,
+  category_attributes: [
+    CategoryAttribute.new(presence: false, category: core)
+  ],
+  description: 'A persistent, non-reassigned, privacy-preserving identifier for
+  a user shared between an identity provider and service provider. An identity
+  provider uses the appropriate value of this attribute when communicating with
+  a particular service provider or group of service providers, and does not
+  reveal that value to any other service provider except in limited
+  circumstances.
+<br /><br />
+ Persistence: Does not require a specific lifetime, but the
+ association should be maintained longer than a single user interaction and long
+ enough to be useful as a key for a particular service that is consuming it.
+<br /><br />
+ Privacy: This attribute is designed to preserve the user\'s privacy and
+ inhibit the ability of multiple unrelated services from correlating user
+ activity by comparing values. It is therefore required to be opaque.
+<br /><br />
+ Uniqueness: A value of this attribute is intended only for consumption by a
+ specific audience of applications (often a single one). Values of this
+ attribute therefore must be unique within the namespace of the identity
+ provider and the namespace of the service provider(s) for whom the value
+ is created. The value is "qualified" by these two namespaces and need not
+ be unique outside them. Logically, the attribute value is made up of the
+ triple of an identifier, the identity provider, and the service provider(s).
+<br /><br />
+ Reassignment: A distinguishing feature of this attribute is that it prohibits
+ reassignment. Since the values are opaque, there is no meaning attached to any
+ particular value beyond its identification of the user. Therefore particular
+ values created by an identity provider must not be  reassigned such that the
+ same value given to a particular',
+  notes_on_format: 'An opaque string of no more than 256 characters.
+<br /><br />
+The format comprises the entity name of the identity provider, the entity name
+ of the service provider, and the opaque string value. These strings are
+ separated by “!” symbols.',
+  notes_on_usage: 'Service Providers should use this attribute to support
+aspects of its
+ service that depend on recognising the same user from session to session.
+ The most common use is to enable service personalisation, to record user
+ preferences such as stored search expressions across user sessions. A secondary
+ use is to enable tracking of user activity, to make it easier to detect
+ systematic downloading of content or other suspected breaches of licence
+ conditions.
+<br /><br />
+ The attribute enables an organisation to provide a persistent, opaque, user
+ identifier to a service provider. For each user, the identity provider presents
+ a different value to each service provider to which the
+ attribute is released.',
+  notes_on_privacy: 'This attribute is intended to be a privacy-preserving
+   attribute.'
+)
+
 faas = %w(
   mail
   email
