9a12c82734ed50878ce466da89ee5a6cdbd0424e
Ryan Caught <rcaught@gmail.com>
2017-08-15 14:16:59 +1000

    Lockdown secure headers for Rails app

--- config/initializers/secure_headers_validator.rb
+++ config/initializers/secure_headers_validator.rb
@@ -9,18 +9,10 @@ app_config = Rails.application.config.validator_service
 SecureHeaders::Configuration.override(:default) do |config|
   config.csp[:object_src] = ["'self'"]
 
-  config.csp[:script_src] = config.csp[:script_src] + [
-    'https://www.google.com/',
-    "'unsafe-inline'",
-    "'unsafe-eval'",
-    'http://www.google-analytics.com/analytics.js',
-    'data:'
-  ]
   config.csp[:style_src] = config.csp[:style_src] + [
-    "'unsafe-inline'",
-    'https://www.google.com',
-    'https://ajax.googleapis.com'
+    "'unsafe-inline'"
   ]
+
   config.csp[:report_uri] << app_config.secure_headers[:csp][:report_uri]
 end
 
