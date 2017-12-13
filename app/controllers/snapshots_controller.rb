c9690f5fe46be4cc4c05da7c423077e449d29f10
Bradley Beddoes <bradleybeddoes@gmail.com>
2017-12-13 10:20:43 +1000

    Fix Rubocop Style/DateTime

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -8,7 +8,7 @@ class SnapshotsController < ApplicationController
     return redirect_to(root_path) unless session[:attributes]
 
     @failed = true
-    @snapshot = Snapshot.new(created_at: DateTime.current)
+    @snapshot = Snapshot.new(created_at: Time.current)
     @snapshot = Snapshot.assign_attributes(session[:attributes], @snapshot)
 
     show_actions
